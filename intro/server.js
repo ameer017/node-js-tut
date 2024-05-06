5fe71b42291c8a8a3545e66404e2714fe445173b
shellback-hacker-house <rajiabdullahi907@yahoo.com>
2024-05-06 14:25:44 +0100

    appending-file

--- intro/server.js
+++ intro/server.js
@@ -2,15 +2,54 @@
 
 // --> File system - CRUD
 const fs = require("fs");
+const path = require("path");
 
 // with to string method
-fs.readFile("./files/starter.txt", (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
-});
+// fs.readFile("./files/starter.txt", (err, data) => {
+//   if (err) throw err;
+//   console.log(data.toString());
+// });
 
 // without to string method
-fs.readFile("./files/starter.txt", "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
+// fs.readFile("./files/starter.txt", "utf8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// instead of hardcoding the file path directly into the fs setup, we can use path module.
+fs.readFile(
+  path.join(__dirname, "files", "starter.txt"),
+  "utf8",
+  (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  }
+);
+
+// write file
+fs.writeFile(
+  path.join(__dirname, "files", "reply.txt"),
+  "Nice to Meet you 🌱.",
+  (err) => {
+    if (err) throw err;
+    console.log("Write complete");
+  }
+);
+
+// update
+fs.appendFile(
+  path.join(__dirname, "files", "test.txt"),
+  "Testing test....",
+  (err) => {
+    if (err) throw err;
+    console.log("Append complete");
+  }
+);
+
+console.log("Hello....");
+// exit on uncaught errors
+
+process.on("uncaughtException", (err) => {
+  console.error(`There was an uncaught error: ${err}`);
+  process.exit(1);
 });
