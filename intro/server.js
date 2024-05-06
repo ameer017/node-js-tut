71c8b6a54c0a9d0bad1f91555ede53ed6800d1ba
shellback-hacker-house <rajiabdullahi907@yahoo.com>
2024-05-06 13:19:24 +0100

    file storage

--- intro/server.js
+++ intro/server.js
@@ -1,1 +1,16 @@
-console.log("Hello node");
+// console.log("Hello node");
+
+// --> File system - CRUD
+const fs = require("fs");
+
+// with to string method
+fs.readFile("./files/starter.txt", (err, data) => {
+  if (err) throw err;
+  console.log(data.toString());
+});
+
+// without to string method
+fs.readFile("./files/starter.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
