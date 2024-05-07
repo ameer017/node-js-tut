63e40a53e4fd24a21eaef1d3d0f82a874fdfb4ce
shellback-hacker-house <rajiabdullahi907@yahoo.com>
2024-05-07 13:52:04 +0100

    event-emitter

--- npm-package/index.js
+++ npm-package/index.js
@@ -1,5 +1,15 @@
-const { format } = require("date-fns");
-const {v4: uuid} = require("uuid")
+const logEvents = require("./logEvent");
 
-console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
-console.log(uuid())
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+//add listener for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  myEmitter.emit("log", "Log event emitted");
+}, 2000);
