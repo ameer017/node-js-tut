ec826c9d1fd0e8c1050493d9c7eded773db98293
ameer017 <rajiabdullahi907@gmail.com>
2024-05-12 09:14:00 +0100

    code-tweak

--- express-routing/routes/api/employees.js
+++ express-routing/routes/api/employees.js
@@ -1,9 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const data = {};
+data.employees = require("../../data/employees.json");
 
-router.get("/", (req, res) => { 
-    res.sendFile(path.join(__dirname, "..", "..", "data", "employees.json"))
-})
+router
+  .route("/")
+  .get((req, res) => {
+    res.json(data.employees);
+  })
+  .post((req, res) => {
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      role: req.body.role,
+    });
+  })
+  .put((req, res) => {
+    res.json({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      role: req.body.role,
+    });
+  })
+  .delete((req, res) => {
+    res.json({ id: req.body.id });
+  });
+// router.get("/", (req, res) => {
+//     res.sendFile(path.join(__dirname, "..", "..", "data", "employees.json"))
+// })
 
 module.exports = router;
